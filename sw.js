b3d9ba832d204a08ad39cb11ae8f85362573ddd6
danielfuchsnuernberg <daniel.fuchs.nuernberg@web.de>
2026-07-22 21:23:43 +1000

    v53

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
    Bump CACHE on every version so devices pull the new copy instead
    of serving an old cached one.
    ============================================================ */
-const CACHE = 'nmplotter-iphone-v46';
+const CACHE = 'nmplotter-iphone-v53';
 
 const SHELL = [
   './',
