5431a99f27d43c82ea1ff61dcc8074e693302edf
danielfuchsnuernberg <daniel.fuchs.nuernberg@web.de>
2026-07-31 10:42:03 +1000

    v89

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@
    Bump CACHE on every version so devices pull the new copy instead
    of serving an old cached one.
    ============================================================ */
-const CACHE         = 'nmplotter-iphone-v88';
+const CACHE         = 'nmplotter-iphone-v89';
 const TERRAIN_CACHE = 'nmplotter-iphone-terrain';
 
 const SHELL = [
